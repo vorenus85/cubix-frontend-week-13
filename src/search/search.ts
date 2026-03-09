36dabd40e0b2fd221f8988d4f924969640b66e16
János Perge <janos.perge@gmail.com>
2026-03-09 16:05:37 +0100

    use scss theme feature, add loading indicator

--- src/search/search.ts
+++ src/search/search.ts
@@ -6,11 +6,17 @@ export class SearchFrom {
   cityInput: HTMLInputElement;
   dateInput: HTMLInputElement;
   submitBtn: HTMLButtonElement;
+  cardContainer: HTMLElement;
+  errorMessage: HTMLElement;
 
   constructor(private readonly weatherService: WeatherService) {
     this.form = document.getElementById('weather-form') as HTMLFormElement;
     this.cityInput = document.getElementById('city-input') as HTMLInputElement;
     this.dateInput = document.getElementById('date-input') as HTMLInputElement;
+    this.cardContainer = document.getElementById(
+      'weather-container',
+    ) as HTMLElement;
+    this.errorMessage = document.getElementById('error-message') as HTMLElement;
     this.submitBtn = document.getElementById(
       'weather-submit',
     ) as HTMLButtonElement;
@@ -23,6 +29,12 @@ export class SearchFrom {
     this.form.addEventListener('submit', async (e) => {
       e.preventDefault();
 
+      this.cardContainer.insertAdjacentHTML(
+        'afterbegin',
+        `<div id="loader-container" class="loader-container"><span class="loader"></span></div>`,
+      );
+
+      this.errorMessage.style.display = 'none';
       this.submitBtn.disabled = true;
 
       try {
@@ -40,8 +52,16 @@ export class SearchFrom {
         this.form.reset();
       } catch (error) {
         console.log(error);
+        this.errorMessage.style.display = 'block';
+        setTimeout(() => {
+          this.errorMessage.style.display = 'none';
+        }, 2000);
+
         throw error;
       } finally {
+        this.cardContainer.removeChild(
+          document.getElementById('loader-container')!,
+        );
         this.submitBtn.disabled = false;
       }
     });
